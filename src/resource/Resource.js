cd12addc7c1b9bba37f09031638b0f85eca82312
João Vieira <joao@evrythng.com>
2017-02-08 13:40:14 +0000

    feat(resource): complete Resource module.

--- src/resource/Resource.js
+++ src/resource/Resource.js
@@ -1,19 +1,32 @@
 import isString from 'lodash-es/isString'
+import isFunction from 'lodash-es/isFunction'
+import isArray from 'lodash-es/isArray'
 import Scope from '../scope/Scope'
 import Entity from '../entity/Entity'
+import api from '../api'
 
 /**
  * A Resource is the base class that implements the CRUD methods behavior.
- * All resource requests are scoped (i.e. they use the scope's API Key.
+ * All resource requests are scoped (i.e. they use the scope's API Key).
  *
  * Every resource operation tries to serialize/deserialize the type of object
  * corresponding to the resource (i.e. when creating a Thng, the developer gets
- * a Thng entity back, with nested methods.
+ * a Thng entity back, with nested methods).
  *
  * @export
  * @class Resource
  */
 export default class Resource {
+  /**
+   * A Resource requires a Scope sub-class (App, Operator, etc.) and the
+   * corresponding path in the Engine API. An Entity sub-class can be
+   * provider, in which case is it used to create instance of that Entity class
+   * when returning from API requests.
+   *
+   * @param {Scope} scope - Scope containing API Key
+   * @param {string} path - Relative path to API resource
+   * @param {Entity} [entity] - Reference to Entity class (constructor)
+   */
   constructor (scope, path, entity) {
     if (!(scope && scope instanceof Scope)) {
       throw new TypeError('Scope should inherit from Scope (e.g. EVT.App).')
@@ -36,11 +49,146 @@ export default class Resource {
     }
   }
 
-  update () {
-
+  /**
+   * Convert an Entity instance into plain JSON payload for API requests.
+   *
+   * @param {Entity} entity - Instace of entity object
+   * @returns {Object}
+   */
+  serialize (entity = {}) {
+    if (!(this.entity && entity instanceof this.entity)) {
+      return entity
+    }
+    return entity.json()
   }
 
-  ['delete'] () {
+  /**
+   * Opposite of serialization. Convert API responses into proper Entities
+   * using the entity class of the resource. If response is a collection,
+   * converts each of the items.
+   *
+   * @param {Object} response - JSON response from API
+   * @returns {Object|Entity|Response} - Entity if able to deserialize;
+   * Response if fullResponse option was used; Object otherwise.
+   */
+  deserialize (response) {
+    if (response && this.entity) {
+      if (isArray(response)) {
+        return response.map(this.deserialize)
+      }
 
+      if (response.body) {
+        // Full response, return promise with deserialized JSON body.
+        response.json = Promise.resolve(response.json()).then(this.deserialize)
+      } else {
+        // JSON response, base case.
+        // Create new entity with updated resource derived from current.
+        let newPath = this.path
+
+        // Expand resource path with ID of entity.
+        if (response.id && newPath.indexOf(response.id) === -1) {
+          newPath += `/${response.id}`
+        }
+
+        const newResource = new Resource(this.scope, newPath, this.entity)
+        const EntityClass = this.entity
+        return new EntityClass(response, newResource)
+      }
+    }
+
+    return response
+  }
+
+  /**
+   * Create a new entity for this resource.
+   *
+   * @param {Object|Entity} body - Entity to create
+   * @param {Object} [options] - Options of the request
+   * @param {Function} [callback] - Error-first callback
+   * @returns {Promise}
+   */
+  create (body, options, callback) {
+    if (!body || isFunction(body)) {
+      throw new TypeError('Create method must have payload.')
+    }
+
+    return this._request({ method: 'post' }, options, callback)
+  }
+
+  /**
+   * Reads resource entities.
+   *
+   * @param {Object} [options] - Options of the request
+   * @param {Function} [callback] - Error-first callback
+   * @returns {Promise}
+   */
+  read (options, callback) {
+    return this._request({ method: 'get' }, options, callback)
+  }
+
+  /**
+   * Updates entity via this resource.
+   *
+   * @param {Object|Entity} body - Entity to create
+   * @param {Object} [options] - Options of the request
+   * @param {Function} [callback] - Error-first callback
+   * @returns {Promise}
+   */
+  update (body, options, callback) {
+    if (!body || isFunction(body)) {
+      throw new TypeError('Update method must have payload.')
+    }
+
+    return this._request({ method: 'put' }, options, callback)
+  }
+
+  /**
+   * Deletes entity in resource.
+   *
+   * @param {Object} [options] - Options of the request
+   * @param {Function} [callback] - Error-first callback
+   * @returns {Promise}
+   */
+  ['delete'] (options, callback) {
+    return this._request({ method: 'delete' }, options, callback)
+  }
+
+  // PRIVATE
+
+  /**
+   * Actual request call. This method merges mandatory request options with
+   * user provided ones and forces the resource path and API Key, so they are
+   * not overriden. Serializes any body passed in, and deserializes response in
+   * the end.
+   *
+   * Callback may be used in place of userOptions.
+   *
+   * @param {Objects} requestOptions - Mandatory request options
+   * @param {Object} [userOptions] - Optional user options
+   * @param {Function} [callback] - Error-first callback
+   * @returns {Promise.<Object|Entity|Response>}
+   * @private
+   */
+  _request (requestOptions, userOptions, callback) {
+    if (isFunction(userOptions)) {
+      callback = userOptions
+    }
+
+    // Merge options, priority to mandatory ones.
+    const options = Object.assign(
+      userOptions,
+      requestOptions,
+      {
+        url: this.path,
+        apiKey: this.scope.apiKey
+      }
+    )
+
+    // Serialize Entity into JSON payload.
+    if (options.body) {
+      options.body = this.serialize(options.body)
+    }
+
+    return api(options, callback).then(this.deserialize)
   }
 }
