719649e8d35dcc9f9acb3b22c9c9347427298e5d
C-D-Lewis <Bonsitm@gmail.com>
2019-04-16 13:45:26 +0100

    Add project and application tests

---
+++ test/e2e/entity/projects.spec.js
@@ -0,0 +1,48 @@
+const { expect } = require('chai');
+const { getScope } = require('../util');
+
+const PROJECT_NAME = 'test';
+
+module.exports = (scopeType) => {
+  describe('Projects', () => {
+    let scope, project;
+
+    before(() => {
+      scope = getScope(scopeType);
+    });
+
+    it('should create a project', async () => {
+      project = await scope.project().create({ name: PROJECT_NAME });
+
+      expect(project).to.be.an('object');
+      expect(project.name).to.equal(PROJECT_NAME);
+      expect(project.id).to.have.length(24);
+    });
+
+    it('should read all projects', async () => {
+      const res = await scope.project().read();
+
+      expect(res).to.be.an('array');
+      expect(res).to.have.length.gte(1);
+    });
+
+    it('should read a project', async () => {
+      const res = await scope.project(project.id).read();
+
+      expect(res).to.be.an('object');
+      expect(res.id).to.equal(project.id);
+    });
+
+    it('should update a project', async () => {
+      const payload = { tags: ['updated'] };
+      const res = await scope.project(project.id).update(payload);
+      
+      expect(res).to.be.an('object');
+      expect(res.tags).to.deep.equal(payload.tags);
+    });
+
+    it('should delete a project', async () => {
+      await scope.project(project.id).delete();
+    });
+  });
+};
--- test/e2e/index.spec.js
+++ test/e2e/index.spec.js
@@ -14,14 +14,14 @@ describe('evrythng.js', () => {
   });
 
   describe('as anonymous Application User', () => {
-    require('./entity/thngs.spec')('anonUser');
-    require('./entity/products.spec')('anonUser');
+    require('./entity/actions.spec')('anonUser');
+    require('./entity/actionTypes.spec')('anonUser');
     require('./entity/collections.spec')('anonUser');
     require('./entity/places.spec')('anonUser');
-    require('./entity/actionTypes.spec')('anonUser');
-    require('./entity/actions.spec')('anonUser');
-    require('./entity/properties.spec')('anonUser', 'thng');
+    require('./entity/products.spec')('anonUser');
     require('./entity/properties.spec')('anonUser', 'product');
+    require('./entity/properties.spec')('anonUser', 'thng');
+    require('./entity/thngs.spec')('anonUser');
 
     after(async () => {
       const operator = getScope('operator');
@@ -32,26 +32,28 @@ describe('evrythng.js', () => {
   });
 
   describe('as Trusted Application', () => {
-    require('./entity/thngs.spec')('trustedApplication');
-    require('./entity/products.spec')('trustedApplication');
+    require('./entity/actions.spec')('trustedApplication');
+    require('./entity/actionTypes.spec')('trustedApplication');
     require('./entity/collections.spec')('trustedApplication');
     require('./entity/places.spec')('trustedApplication');
-    require('./entity/actionTypes.spec')('trustedApplication');
-    require('./entity/actions.spec')('trustedApplication');
-    require('./entity/properties.spec')('trustedApplication', 'thng');
+    require('./entity/products.spec')('trustedApplication');
     require('./entity/properties.spec')('trustedApplication', 'product');
+    require('./entity/properties.spec')('trustedApplication', 'thng');
+    require('./entity/thngs.spec')('trustedApplication');
   });
 
   describe('as Operator', () => {
-    require('./entity/thngs.spec')('operator');
-    require('./entity/products.spec')('operator');
-    require('./entity/collections.spec')('operator');
-    require('./entity/user.spec')('operator');
-    require('./entity/places.spec')('operator');
-    require('./entity/actionTypes.spec')('operator');
     require('./entity/actions.spec')('operator');
-    require('./entity/properties.spec')('operator', 'thng');
+    require('./entity/actionTypes.spec')('operator');
+    require('./entity/applications.spec')('operator');
+    require('./entity/collections.spec')('operator');
+    require('./entity/places.spec')('operator');
+    require('./entity/products.spec')('operator');
+    require('./entity/projects.spec')('operator');
     require('./entity/properties.spec')('operator', 'product');
+    require('./entity/properties.spec')('operator', 'thng');
+    require('./entity/thngs.spec')('operator');
+    require('./entity/user.spec')('operator');
 
     after(async () => {
       const operator = getScope('operator');
