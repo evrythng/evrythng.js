2830fbbc13a48307bc909a4f248b4410a958188f
C-D-Lewis <Bonsitm@gmail.com>
2019-05-28 13:29:07 +0100

    Add plugin api resources, update README.md

--- src/use.js
+++ src/use.js
@@ -4,6 +4,30 @@ import Operator from './scope/Operator'
 import TrustedApplication from './scope/TrustedApplication'
 import User from './scope/User'
 
+import Access from './entity/Access'
+import Account from './entity/Account'
+import Action from './entity/Action'
+import ActionType from './entity/ActionType'
+import ApplicationEntity from './entity/Application'
+import AppUser from './entity/AppUser'
+import Batch from './entity/Batch'
+import Collection from './entity/Collection'
+import File from './entity/File'
+import Location from './entity/Location'
+import Permission from './entity/Permission'
+import Place from './entity/Place'
+import Product from './entity/Product'
+import Project from './entity/Project'
+import Property from './entity/Property'
+import ReactorSchedule from './entity/ReactorSchedule'
+import ReactorScript from './entity/ReactorScript'
+import Redirection from './entity/Redirection'
+import Redirector from './entity/Redirector'
+import Resource from './resource/Resource'
+import Role from './entity/Role'
+import Thng from './entity/Thng'
+import UserEntity from './entity/User'
+
 /**
  * The items that the plugin may access and manipulate to install new functionality.
  * 
@@ -16,6 +40,31 @@ const API = {
     TrustedApplication,
     User,
     Device
+  },
+  entities: {
+    Action,
+    ActionType,
+    Application: ApplicationEntity,
+    AppUser,
+    Batch,
+    Collection,
+    File,
+    Location,
+    Permission,
+    Place,
+    Product,
+    Project,
+    Property,
+    ReactorSchedule,
+    ReactorScript,
+    Redirection,
+    Redirector,
+    Role,
+    Thng,
+    User: UserEntity
+  },
+  resources: {
+    Resource
   }
 }
 
@@ -25,11 +74,11 @@ const API = {
  * The plugin is provided an API object with items for it to manipulate in its
  * `install()` method, such as adding new methods to scope prototypes.
  *
- * @param {object} plugin - The plugin object, which must export the `install()` method.
+ * @param {object} plugin - The plugin object, which must implement the `install()` method.
  */
 export default function use (plugin) {
   if (!plugin.install || typeof plugin.install !== 'function') {
-    throw new Error('Plugin must export an \'install\' method')
+    throw new Error('Plugin must implement an \'install()\' method')
   }
 
   try {
--- test/e2e/misc/use.spec.js
+++ test/e2e/misc/use.spec.js
@@ -6,23 +6,42 @@ const TestPlugin = {
   install: (api) => {
     // Extend the Operator scope API
     api.scopes.Operator.prototype.getFoo = () => 'foo'
+
+    // Add functionality to a Thng
+    api.entities.Thng.prototype.getSummary = function () {
+      return `${this.name} (${this.id})`
+    }
   }
 }
 
 module.exports = () => {
   describe('use', () => {
-    let operator
+    let operator, thng
 
-    before(() => {
+    before(async () => {
       operator = getScope('operator')
+
+      thng = await operator.thng().create({ name: 'test' })
     })
 
-    it('should install a plugin', () => {
-      evrythng.use(TestPlugin)
+    after(async () => {
+      await operator.thng(thng.id).delete()
+    })
 
+    it('should not throw when installing a plugin', async () => {
+      evrythng.use(TestPlugin)
+    })
+
+    it('should extend a scope with a new method', async () => {
       expect(evrythng.Operator.prototype.getFoo).to.be.a('function')
       expect(operator.getFoo).to.be.a('function')
       expect(operator.getFoo()).to.equal('foo')
+    })
+
+    it('should expect an entity with a new method', async () => {
+      const expected = `${thng.name} (${thng.id})`
+      expect(thng.getSummary).to.be.a('function')
+      expect(thng.getSummary()).to.equal(expected)
     })
   })
 }
